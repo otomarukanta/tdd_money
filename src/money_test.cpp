a25fb4d1b6f7f6f1da7a9dd743944a6d6b679b57
otomarukanta <kanta208@gmail.com>
2014-09-09 02:57:55 +0900

    delete Dollar() and Franc()

--- src/money_test.cpp
+++ src/money_test.cpp
@@ -12,22 +12,13 @@ TEST(FrancTest, Multiplication) {
 	ASSERT_TRUE(Money::franc(10)->equals(five->times(2)));
 	ASSERT_TRUE(Money::franc(15)->equals(five->times(3)));
 }
-TEST(DollarTest, Equality) {
+TEST(MoneyTest, Equality) {
 	ASSERT_TRUE (Money::dollar(5)->equals(Money::dollar(5)));
 	ASSERT_FALSE(Money::dollar(5)->equals(Money::dollar(6)));
-} 
-TEST(FrancTest, Equality) {
-	ASSERT_TRUE (Money::franc(5)->equals(Money::franc(5)));
-	ASSERT_FALSE(Money::franc(5)->equals(Money::franc(6)));
-}
-TEST(MoneyTest, Equality) {
 	ASSERT_FALSE(Money::franc(5)->equals(Money::dollar(5)));
-}
+} 
 
 TEST(MoneyTest, Currency) {
 	ASSERT_EQ("USD", Money::dollar(1)->currency());
 	ASSERT_EQ("CHF", Money::franc(1)->currency());
-}
-TEST(MoneyTest, Different) {
-	ASSERT_TRUE(Money(10, "CHF").equals(Money::franc(10)));
 }
